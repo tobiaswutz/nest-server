906df7a090228f38db8f3ebaf33a6a90b6e0facc
Tobias <tobiaswutz22@gmail.com>
2022-07-08 20:41:02 +0200

    initial

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  public login(): void {
+    // ...
+  }
+
+  public signup(): void {
+    // ...
+  }
+}
