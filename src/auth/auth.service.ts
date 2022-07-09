6d1dd69a1dbb4ce93dae86b7cf0c11448cded99c
Tobias <tobiaswutz22@gmail.com>
2022-07-09 19:01:03 +0200

    finished user auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,49 +3,40 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import { domainToASCII } from 'url';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) { }
 
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
     try {
-      const user = await this.prisma.user.create({
-        data: {
-          email: dto.email,
-          hash,
-        },
-      });
-      delete user.hash;
-      return user;
+      const user = await this.prisma.user.create({ data: { email: dto.email, hash, }, });
+      return this.signToken(user.id, user.email);
     } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ForbiddenException('Email already in use');
-        }
-      }
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') { throw new ForbiddenException('Email already in use'); }
       throw error;
     }
   }
 
   async signin(dto: AuthDto) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email: dto.email,
-      },
-    });
-    if (!user) {
-      throw new ForbiddenException('Invalid credentials');
-    }
+    const user = await this.prisma.user.findUnique({ where: { email: dto.email, }, });
+    if (!user) { throw new ForbiddenException('Invalid credentials'); }
 
     const pwMatches = await argon.verify(user.hash, dto.password);
-    if (!pwMatches) {
-      throw new ForbiddenException('Invalid credentials');
-    }
+    if (!pwMatches) { throw new ForbiddenException('Invalid credentials'); }
+    return this.signToken(user.id, user.email);
+  }
 
-    delete user.hash;
-    return user;
+  async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+    const payload = { sub: userId, email };
+    const token = await this.jwt.signAsync(payload, { expiresIn: '2h', secret: this.config.get('JWT_SECRET') });
+    return { access_token: token };
   }
 }
